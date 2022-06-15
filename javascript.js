0ff3ba82e797d5edf157fb269dfa59ef650299ff
enryMine <104450687+enryMine@users.noreply.github.com>
2022-06-15 13:35:53 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//freccina top
+function scrollToTop() {
+           window.scrollTo(0, 0);
+        }
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
